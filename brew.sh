c15e2e35ed6a3f57cde81ea7e3cffbfd742cadb3
Alex Lu <alxlu@users.noreply.github.com>
2017-05-19 22:48:15 -0700

    switch to brew installed bash as default shell

--- brew.sh
+++ brew.sh
@@ -36,6 +36,12 @@ chsh -s /usr/local/bin/bash
 brew tap homebrew/versions
 brew install bash-completion2
 
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
