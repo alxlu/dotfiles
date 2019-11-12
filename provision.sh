04d4c584219858f6af4ee1df08807eeb9d9ab55f
Alex Lu <alxlu@users.noreply.github.com>
2019-11-11 17:16:04 -0800

    add neovim linking

--- provision.sh
+++ provision.sh
@@ -2,3 +2,8 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
