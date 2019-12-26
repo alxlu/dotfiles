7ba7f13c65d51d2f0aeac20c84dc07e5ac4173ba
Alex Lu <alxlu@users.noreply.github.com>
2019-12-25 23:16:13 -0800

    fix debian install scripts

--- debian.sh
+++ debian.sh
@@ -2,28 +2,29 @@
 
 sudo -v
 
-sudo apt update
+sudo apt-get update
 
 # Install zsh
-sudo apt install zsh -y
+sudo apt-get install zsh -y
 
 # Install oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # Install zplug
 export ZPLUG_HOME=~/.zplug
+sudo apt-get install git -y
 git clone https://github.com/zplug/zplug.git $ZPLUG_HOME
 
 # Install fzf
-sudo apt install fzf -y
+sudo apt-get install fzf -y
 
 # Install fasd
-sudo apt install make unzip -y
+sudo apt-get install make unzip -y
 mkdir fasd_tmp
 cd fasd_tmp
 curl -LO https://github.com/clvv/fasd/zipball/1.0.1
 unzip -j 1.0.1
-make install
+sudo make install
 cd ../
 rm -rf fasd_tmp
 
