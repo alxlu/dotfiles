cf92d98eb60eebb9a7f1563e7de51a1f0866d2e1
Alex Lu <alxlu@users.noreply.github.com>
2020-05-12 14:59:20 -0700

    update curl script

--- curl.sh
+++ curl.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-case "$OSTYPE" in
-  darwin*)
-    cd; curl -#L https://github.com/alxlu/dotfiles/tarball/v2 | tar -xzv --strip-components 1 --exclude={i3setup.sh,debian.sh,linkbin.sh,bootstrap.sh,curl.sh}
-  ;;
-  linux*)
-    cd; curl -#L https://github.com/alxlu/dotfiles/tarball/v2 | tar -xzv --strip-components 1 --exclude={brew.sh,macos.sh,linkbin.sh,bootstrap.sh,curl.sh}
-  ;;
-esac
+main() {
+  case "$OSTYPE" in
+    darwin*)
+      cd; curl -#L https://github.com/alxlu/dotfiles/tarball/master | tar -xzv --strip-components 1 --exclude={i3setup.sh,debian.sh,linkbin.sh,bootstrap.sh,curl.sh}
+      ;;
+    linux*)
+      cd; curl -#L https://github.com/alxlu/dotfiles/tarball/master | tar -xzv --strip-components 1 --exclude={brew.sh,macos.sh,linkbin.sh,bootstrap.sh,curl.sh}
+      ;;
+  esac
+}
+
+main
