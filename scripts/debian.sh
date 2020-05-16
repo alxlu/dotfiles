708a16c75aed6426214f2e5e40d347a153400e3f
Alex Lu <alxlu@users.noreply.github.com>
2020-05-15 18:51:58 -0700

    cleanup debian provisioning script

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -7,28 +7,10 @@ sudo apt-get update
 # Install zsh
 sudo apt-get install zsh -y
 
-# Install oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-
-# Install zplug
-export ZPLUG_HOME=$HOME/.zplug
-sudo apt-get install git -y
-git clone https://github.com/zplug/zplug.git $ZPLUG_HOME
-
-# Install fzf
+# Install fzf and fasd
 sudo apt-get install fzf -y
-
-# Install fasd
-sudo apt-get install make unzip -y
-mkdir fasd_tmp
-cd fasd_tmp
-curl -LO https://github.com/clvv/fasd/zipball/1.0.1
-unzip -j 1.0.1
-sudo make install
-cd ../
-rm -rf fasd_tmp
+sudo apt-get install fasd -y
 
 sudo chsh -s $(which zsh)
 
-export ZPLUG_AUTO=true
 exec zsh
